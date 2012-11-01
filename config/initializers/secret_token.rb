ebd0932185a417eebf4fd77817f4a0a4b2621fdf
Douglas Safreno <dsafreno@stanford.edu>
2012-11-01 00:50:00 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gusto::Application.config.secret_token = '7fdba431929260edff1bc593181538239ce7396d25c4286a53a8f0286f807817970c9221a82208294ea910f0a6ebd451915cc06b208575173086214d77042bd0'
